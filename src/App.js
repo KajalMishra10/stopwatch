90063974f89d5be950911bf3e93e563c3e472c5f
Kajal <kajana@amdocs.com>
2024-03-17 10:47:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        if (seconds === 59) {
+          setMinutes((prevMinutes) => prevMinutes + 1);
+          setSeconds(0);
+        } else {
+          setSeconds((prevSeconds) => prevSeconds + 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning, seconds]);
+
+  const startStopwatch = () => {
+    setIsRunning(true);
+  };
+
+  const stopStopwatch = () => {
+    setIsRunning(false);
+  };
+
+  const resetStopwatch = () => {
+    setIsRunning(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <p>
+        Time:{" "}
+        <span>
+          {String(minutes).padStart(1, "0")}:{String(seconds).padStart(2, "0")}
+        </span>
+      </p>
+      <div>
+        {!isRunning ? (
+          <button onClick={startStopwatch}>Start</button>
+        ) : (
+          <button onClick={stopStopwatch}>Stop</button>
+        )}
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
+    </div>
+  );
+}
